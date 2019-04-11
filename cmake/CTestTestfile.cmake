924e1690c9ba4e71f7926d3864c094490a00ecb5
Fakhrigv <faxri1999@Mail.ru>
2019-04-11 23:46:59 +0400

    second

---
+++ cmake/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/CMake/cmake
+# Build directory: /home/user/Desktop/CMake/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(sum_test1 "/home/user/Desktop/CMake/cmake/bin/sample")
